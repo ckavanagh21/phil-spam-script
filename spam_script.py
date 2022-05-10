af25724d999682351b8521ddc62b0640574a787a
ckavanagh21 <50001615+ckavanagh21@users.noreply.github.com>
2022-05-10 15:39:37 -0700

    Added original file

---
+++ spam_script.py
@@ -0,0 +1,14 @@
+from re import A
+import keyboard
+import time
+import random
+#corpus.download('words')
+
+time.sleep(5)
+
+for i in range(100):
+    keyboard.write("Pi Kapp")
+    keyboard.press("enter")
+    keyboard.release("enter")
+    time.sleep(0.5)
+
