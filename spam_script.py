60a8e03a4711a707f7f790266fbe31b80b46074b
ckavanagh21 <kavanagh.colinp@gmail.com>
2022-05-10 16:22:28 -0700

    added features
    added two features, one prints "pikapp" multiple times while the other prints a list of numbers. also added a feature that allows the user to specify how many times they want something to print.

--- spam_script.py
+++ spam_script.py
@@ -1,14 +1,31 @@
-from re import A
 import keyboard
 import time
 import random
-#corpus.download('words')
+programSelection = str(input("Which program would you like to run?\nPlease enter the number of the program:\n1: Repeat 'Pi Kapp' a given number of times (not unique)\n2: Print a list of numbers, with all numbers being unique\n"))
+numberOfRepetitions = int(input("How many times would you like to send a message? (Pick a number)\n"))
+print("Program starting in in 5...")
+time.sleep(1)
+print("4..")
+time.sleep(1)
+print("3...")
+time.sleep(1)
+print("2..")
+time.sleep(1)
+print("1..")
+time.sleep(1)
+print("Starting program!")
 
-time.sleep(5)
-
-for i in range(100):
-    keyboard.write("Pi Kapp")
-    keyboard.press("enter")
-    keyboard.release("enter")
-    time.sleep(0.5)
-
+if "1" in programSelection:
+    for i in range(numberOfRepetitions):
+       keyboard.write("Pi Kapp")
+       keyboard.press("enter")
+       keyboard.release("enter")
+       time.sleep(0.5)
+elif "2" in programSelection:
+    for i in numberOfRepetitions:
+        keyboard.write(i)
+        keyboard.press("enter")
+        keyboard.release("enter")
+else:
+    print("Error! Please make sure you enter in a proper program number and try again.")
+print("Program finished running! You can close this window.")
