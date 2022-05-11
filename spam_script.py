7443fe1523b88e1d148cbb841794ddee6dc69f6e
ckavanagh21 <kavanagh.colinp@gmail.com>
2022-05-10 18:28:19 -0700

    Update spam_script.py
    added function that prints each word of a sentence individually

--- spam_script.py
+++ spam_script.py
@@ -1,8 +1,11 @@
 import keyboard
 import time
 import random
-programSelection = str(input("Which program would you like to run?\nPlease enter the number of the program:\n1: Repeat 'Pi Kapp' a given number of times (not unique)\n2: Print a list of numbers, with all numbers being unique\n"))
-numberOfRepetitions = int(input("How many times would you like to send a message? (Pick a number)\n"))
+programSelection = str(input("Which program would you like to run?\nPlease enter the number of the program:\n1: Repeat 'Pi Kapp' a given number of times (not unique)\n2: Print a list of numbers, with all numbers being unique\n3: Takes a sentence and prints each word one by one.\n"))
+if "3" in programSelection:
+    text = input("Please enter the text that you would like printed.\n")
+else:
+        numberOfRepetitions = int(input("How many times would you like to send a message?  (Pick a number)\n"))
 print("Program starting in in 5...")
 time.sleep(1)
 print("4..")
@@ -22,10 +25,19 @@ if "1" in programSelection:
        keyboard.release("enter")
        time.sleep(0.5)
 elif "2" in programSelection:
-    for i in numberOfRepetitions:
+    for i in range(numberOfRepetitions):
+        keyboard.write(str(i))
+        keyboard.press("enter")
+        keyboard.release("enter")
+        time.sleep(0.5)
+elif "3" in programSelection:
+    listText = text.split()
+    for i in listText:
         keyboard.write(i)
         keyboard.press("enter")
         keyboard.release("enter")
+        time.sleep(0.5)
 else:
     print("Error! Please make sure you enter in a proper program number and try again.")
 print("Program finished running! You can close this window.")
+
